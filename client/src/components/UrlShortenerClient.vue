53d3d6f41719c4021e89e7b4d2b911fbea37398c
Leonard Herold <git@hdld.me>
2020-04-04 22:19:08 +0200

    Minor UI changes

--- client/src/components/UrlShortenerClient.vue
+++ client/src/components/UrlShortenerClient.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-row>
         <b-col cols="12">
-        <h3>Shorten your URL</h3>
+          <h3>Create a new shortURL</h3>
           <b-form @submit="onSubmit" @reset="onReset" v-if="show">
             <b-form-group
               id="input-group-1"
@@ -17,50 +17,64 @@
           </b-form>
         </b-col>
       </b-row>
-      <hr>
+      <hr />
       <b-row class="mb-4">
-        <b-col cols="7" class="text-left">
-        <h3>Get or delete your url by ID</h3>
+        <b-col cols="12" class="text-left">
+          <h3>Get or Delete Your Existing shortURL</h3>
           <b-form @submit="onShortendSubmit" v-if="show">
-            <b-input-group prepend="ID" class>
-              <b-form-input required v-model="shortendurlcode"></b-form-input>
-              <b-input-group-append>
-                <b-button variant="outline-primary" value="open" type="submit">Get</b-button>
-                <b-button variant="outline-danger" value="delete" type="submit">Delete</b-button>
-              </b-input-group-append>
-            </b-input-group>
+            <b-form-group
+              description="Type the shortURL ID you want to redirect to or delete in the field above."
+            >
+              <b-input-group prepend="ID">
+                <b-form-input required v-model="shortendurlcode"></b-form-input>
+                <b-input-group-append>
+                  <b-button variant="outline-primary" value="open" type="submit">Get</b-button>
+                  <b-button variant="outline-danger" value="delete" type="submit">Delete</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
           </b-form>
         </b-col>
       </b-row>
-      <hr>
+      <hr />
       <b-row class="mb-4">
-        <b-col cols="7" class="text-left">
-        <h3>Update your URL</h3>
-            <b-form @submit="onUpdateSubmit" @reset="onUpdateFormReset" v-if="show">
-              <b-form-group>
-                <b-input-group prepend="ID" class>
-                  <b-form-input required v-model="formUpdate.urlId"></b-form-input>
-                </b-input-group>
-                <b-input-group prepend="New URL" class>
-                  <b-form-input required v-model="formUpdate.newUrl"></b-form-input>
-                </b-input-group>
-              </b-form-group>
-              <b-button type="submit" variant="primary">Submit</b-button>
-            </b-form>
+        <b-col cols="12" class="text-left">
+          <h3>Create Or Update a specific shortURL</h3>
+          <b-form @submit="onUpdateSubmit" @reset="onUpdateFormReset" v-if="show">
+            <b-form-group
+              description="Add your URL and the desired ID of your shortURL in the fields above."
+            >
+              <b-container class="p-0">
+                <b-row>
+                  <b-col cols="4">
+                    <b-input-group prepend="ID">
+                      <b-form-input required v-model="formUpdate.urlId"></b-form-input>
+                    </b-input-group>
+                  </b-col>
+                  <b-col cols="8">
+                    <b-input-group prepend="New URL">
+                      <b-form-input required v-model="formUpdate.newUrl"></b-form-input>
+                    </b-input-group>
+                  </b-col>
+                </b-row>
+              </b-container>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </b-form>
         </b-col>
       </b-row>
-      <hr>
+      <hr />
       <b-row>
         <b-col cols="12">
           <h3>Get a List of ID's or Delete all ID's</h3>
-            <b-button variant="outline-primary" @click="getAll" class="mr-2">List Keys</b-button>
-            <b-modal ref="modal-keys" id="modal-keys" title="Allocated URL Keys" ok-only>
-              <b-container>
-                <b-row v-for="key in keys" :key="key.id">{{key}}</b-row>
-              </b-container>
-            </b-modal>
-            <b-button @click="onDeleteAll" variant="outline-danger">Delete All</b-button>
-          </b-col>
+          <b-button variant="outline-primary" @click="getAll" class="mr-2">List Keys</b-button>
+          <b-modal ref="modal-keys" id="modal-keys" title="Allocated URL Keys" ok-only>
+            <b-container>
+              <b-row v-for="key in keys" :key="key.id">{{key}}</b-row>
+            </b-container>
+          </b-modal>
+          <b-button @click="onDeleteAll" variant="outline-danger">Delete All</b-button>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -80,9 +94,9 @@ export default {
         clickedBtn: ""
       },
       shortendurlcode: "",
-      formUpdate:{
-        urlId:"",
-        newUrl:""
+      formUpdate: {
+        urlId: "",
+        newUrl: ""
       },
       keys: [],
       show: true
@@ -93,11 +107,11 @@ export default {
       evt.preventDefault();
       let request;
       let url = this.form.url;
-      
+
       request = this.api.post(this.form.url);
       request
         .then(resp => {
-          console.log(resp.data)
+          console.log(resp.data);
           let text = `We converted ${url} to the id ${resp.data}`;
 
           this.$bvToast.toast(text, {
@@ -186,30 +200,38 @@ export default {
       evt.preventDefault();
       let request;
 
-      request = this.api.put_by_id(this.formUpdate.urlId, this.formUpdate.newUrl);
+      request = this.api.put_by_id(
+        this.formUpdate.urlId,
+        this.formUpdate.newUrl
+      );
       request
         .then(() => {
-          this.$bvToast.toast("Updated URL of " + this.formUpdate.urlId + " to " + this.formUpdate.newUrl, {
+          this.$bvToast.toast(
+            "Updated URL of " +
+              this.formUpdate.urlId +
+              " to " +
+              this.formUpdate.newUrl,
+            {
               title: "Your Entry was updated",
               toaster: "b-toaster-top-center"
-            });
-        })
-        .catch(err =>{
-          if (err.response.status == 400) {
-              this.$bvToast.toast("This is not a valid URL", {
-                title:
-                  "Invalid URL",
-                variant: "danger",
-                toaster: "b-toaster-top-center"
-              });
-            } else {
-              this.$bvToast.toast("This is not a valid shortURL", {
-                title:
-                  "Invalid shortURL - entry might be deleted or is non-existent",
-                toaster: "b-toaster-top-center"
-              });
             }
+          );
         })
+        .catch(err => {
+          if (err.response.status == 400) {
+            this.$bvToast.toast("This is not a valid URL", {
+              title: "Invalid URL",
+              variant: "danger",
+              toaster: "b-toaster-top-center"
+            });
+          } else {
+            this.$bvToast.toast("This is not a valid shortURL", {
+              title:
+                "Invalid shortURL - entry might be deleted or is non-existent",
+              toaster: "b-toaster-top-center"
+            });
+          }
+        });
     },
     onUpdateFormReset(evt) {
       evt.preventDefault();
