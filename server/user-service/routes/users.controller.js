fcc9ed9135662bb04d0cd926a2fbcabc5e73aac0
luusluus <lucienmartijn@gmail.com>
2020-04-08 19:16:57 +0200

    edit route

--- server/user-service/routes/users.controller.js
+++ server/user-service/routes/users.controller.js
@@ -3,7 +3,7 @@ const { UserService } = require('../services/users.service');
 const userService = new UserService();
 module.exports = app => {
 
-    app.post('/api/users', (req, res) => {
+    app.post('/users', (req, res) => {
         try {
             const user = userService.register(req.body);
             user ? res.status(200).send() : res.status(500).send();
@@ -12,7 +12,7 @@ module.exports = app => {
         }
     });
 
-    app.post('/api/users/login', (req, res) => {
+    app.post('/users/login', (req, res) => {
         const token = userService.authenticate(req.body)
         token ? res.status(200).json(token) : res.status(403).json("forbidden");   
     });
