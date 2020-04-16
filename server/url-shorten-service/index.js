a0d82c43f1dc10e6a4a503c22919bb8ac6af7295
Stijn Meijerink <stijn.meijerink@gmail.com>
2020-04-16 14:01:52 +0200

    Services register at the central register service

--- server/url-shorten-service/index.js
+++ server/url-shorten-service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const constants = require('./config/constants');
-
+const fetch = require('node-fetch');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
    }
  });
 
+
  require('./routes/health.controller')(app);
  require('./routes/urlshortener')(app);
 
@@ -27,5 +28,16 @@ app.use(function(req, res, next) {
     var host = server.address().address
     var port = server.address().port
     
+    // Register
+    const body = {"location": host+":"+port};
+    fetch('http://localhost:8084/service', {
+            method: 'post',
+            body:    JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then(res => res.json())
+        .then(json => console.log("Registered as: " +json))
+        .catch(err => console.log(err));
+
     console.log("Url Shortener Service listening at http://%s:%s", host, port)
  })
--- server/user-service/index.js
+++ server/user-service/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const constants = require('./config/constants');
+const fetch = require('node-fetch');
 
 app.use(bodyParser.json({
     strict:false
@@ -27,5 +28,16 @@ var server = app.listen(constants.PORT, function () {
     var host = server.address().address
     var port = server.address().port
     
+    // Register
+    const body = {"location": host+":"+port};
+    fetch('http://localhost:8084/service', {
+            method: 'post',
+            body:    JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then(res => res.json())
+        .then(json => console.log("Registered as: " +json))
+        .catch(err => console.log(err));
+
     console.log("User Service listening at http://%s:%s", host, port)
 })
